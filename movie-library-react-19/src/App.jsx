20528a6c1b883cbf0f3795a70915388e2f287ee4
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2025-02-26 00:04:25 +0530

    First simple app with props and styles practice

--- movie-library-react-19/src/App.jsx
+++ movie-library-react-19/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Card from './components/card/Card.jsx';
+import Header from './components/Header/Header.jsx';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const name = 'Ritobroto';
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div style={{
+        display: 'flex',
+        flexFlow: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '15px',
+        margin: '0 auto',
+      }}>
+        <Header name={name} />
+        
+        <Card setCount={setCount} />
+        
+        <p className="read-the-docs">
+          Incremented Count is {count}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ movie-library-react-19/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({ name }) => {
+    return (
+        <>
+            <h1 className="flex-1">Hello, { name }</h1>
+        </>
+    );
+}
+
+export default Header;
